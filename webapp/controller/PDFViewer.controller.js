7ad10bc9f6db03adef27784a29364524846ee3f5
P1941269055 <david.vela.tirado@gmail.com>
2019-03-25 09:19:41 +0000

    last changes

--- webapp/controller/PDFViewer.controller.js
+++ webapp/controller/PDFViewer.controller.js
@@ -1,9 +1,21 @@
 sap.ui.define([
-	"nabi/demo/MyLibraryDemoApp/controller/BaseController"
-], function(BaseController) {
+	"nabi/demo/MyLibraryDemoApp/controller/BaseController", 
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("nabi.demo.MyLibraryDemoApp.controller.PDFViewer", {
-
+		onInit: function() {
+			this.getView().setModel(new JSONModel({
+				// pdfsource: ""
+				pdfSource: "static/OpenUI5.pdf" 
+			}));
+			// me.getView().getModel().setProperty("/pdfsource", oEvent.target.result);
+		},
+		onButtonPress: function() {
+			 var path = $.sap.getModulePath("nabi.demo.MyLibraryDemoApp", "/png");
+			 console.log( path )
+			 this.getView().getModel().setProperty("/pdfsource", "static/OpenUI5.pdf" );
+		}
 	});
 });
